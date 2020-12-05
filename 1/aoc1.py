d1d96c8a008f6f5a257a2c319b06ea87e462b795
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-12-04 17:23:57 -0700

    first one down

--- 1/aoc1.py
+++ 1/aoc1.py
@@ -1,2 +1,18 @@
+import itertools
 
-i
+
+def check_constraint(x, y, z):
+    if (x + y + z) == 2020:
+        print(x * y * z)
+
+
+if __name__ == "__main__":
+    expenses = []
+    N = 3
+    # with open('sample.txt', 'r') as inFile:
+    with open('input1.txt', 'r') as inFile:
+        for line in inFile:
+            expenses.append(int(line.strip()))
+
+    for pair in itertools.combinations(expenses, r=N):
+        check_constraint(*pair)
