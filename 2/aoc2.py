9c8f0a612de3fb4f2065253fc196b3fa554b80fe
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-12-04 18:20:46 -0700

    working part 1 of q2

--- 2/aoc2.py
+++ 2/aoc2.py
@@ -1,6 +1,6 @@
 import re
 
-pat = re.compile(r'(\d)-(\d)\s(\w):\s(\w+)')
+pat = re.compile(r'(\d+)-(\d+)\s(\w):\s(\w+)')
 
 
 def parse_requirements(ps):
@@ -13,12 +13,29 @@ def parse_requirements(ps):
     return (low, high, ch, password)
 
 
+def valid(low, high, ch, password):
+    count = password.count(ch)
+    if low <= count <= high:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     passwords = []
     with open('2/sample.txt', 'r') as inFile:
+        # with open('2/input.txt', 'r') as inFile:
         for line in inFile:
             passwords.append(line.strip())
 
-    for p in passwords:
-        res = parse_requirements(p)
-        print(res)
+    # for p in passwords:
+    #     res = parse_requirements(p)
+    #     print(res)
+    #     if valid(*res):
+    #         print("Valid!")
+    #     else:
+    #         print("Not Valid!")
+
+    results = list(
+        filter(None, [valid(*parse_requirements(p)) for p in passwords]))
+    print(len(results))
